80d083c07cfca4949ee8ff3e838483edc67fa401
Hongkun882 <57167935+Hongkun882@users.noreply.github.com>
2024-06-13 14:55:02 -0700

    change static

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -175,7 +175,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/frontend/build/static/"
 MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
