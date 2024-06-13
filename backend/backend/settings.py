f8ae65a51f2623972e463a8acf58acc4563ca572
Hongkun882 <57167935+Hongkun882@users.noreply.github.com>
2024-06-13 14:46:38 -0700

    adding builds

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ecommerce2391.onrender.com']
+ALLOWED_HOSTS = ['ecommerce2391.onrender.com','localhost']
 
 
 # Application definition
