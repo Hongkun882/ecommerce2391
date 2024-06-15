1a9369f22925dfee7792492bd2e660376bd360d6
Hongkun882 <57167935+Hongkun882@users.noreply.github.com>
2024-06-15 01:07:59 -0700

    final fix attemp

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
 
 urlpatterns = [
@@ -29,3 +30,4 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
